1a49066288fb0ac794ca4c43a37f76e2ab9564b7
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-08-20 15:12:24 -0700

    updated correct file path

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,11 +1,11 @@
 const router = require('express').Router()
 const { join } = require('path')
 
-router.get('/*', (req, res) => {
+router.get('/notes', (req, res) => {
     res.sendFile(join(__dirname, '..', 'public', 'notes.html'))
 })
 
-router.get('/', (req, res) => {
+router.get('/*', (req, res) => {
     res.sendFile(join(__dirname, '..', 'public', 'index.html'))
 })
 
